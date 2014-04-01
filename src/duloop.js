cb61f1a85f492d39b0261bc4b10acb3ff3aef1a3
Chris Kelley <chris@vetula.com>
2014-04-01 16:54:52 +0200

    Added feature to save sequences using pouchdb

--- src/duloop.js
+++ src/duloop.js
@@ -1,6 +1,11 @@
 $(function() {
 
+  var db = new PouchDB('SEQdb');
+
   var SEQ = {}
+  SEQ.sessionName = Date.now();
+  console.log("SEQ.sessionName: " + SEQ.sessionName);
+  Backbone.sync = BackbonePouch.sync();
 
 	//	little rec/stop/pause buttons 
 	//	Albino Tonnina 
@@ -10,10 +15,13 @@ $(function() {
 
 
     var is_recording = false;
+    SEQ.currentBar = 0;
     var stoppath = "M21.545,10.917v10.165c0,0.125-0.046,0.234-0.137,0.326c-0.092,0.091-0.2,0.137-0.326,0.137H10.917c-0.125,0-0.233-0.047-0.325-0.137c-0.092-0.092-0.137-0.201-0.137-0.326V10.917c0-0.125,0.045-0.233,0.137-0.325c0.091-0.091,0.2-0.137,0.325-0.137h10.165c0.126,0,0.234,0.045,0.326,0.137C21.499,10.684,21.545,10.792,21.545,10.917z"
     $('#credits ul').append('<li><div id="loops" /></li>');
     $('<svg version="1.1" id="lp_recpause" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="8 8 16 16" enable-background="new 8 8 16 16" xml:space="preserve"><circle fill="#cccccc" cx="16" cy="16" r="6"/>"/>').appendTo('#loops');
     $('<svg version="1.1" style="display:none" id="lp_stop" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="8 8 16 16" enable-background="new 8 8 16 16" xml:space="preserve"><rect x="10.75" y="10.125" width="10.5" height="10.5" fill="#cccccc" /></svg>').appendTo('#loops');
+    $('<svg version="1.1" style="display:none" id="new_seq_button" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="8 8 16 16" enable-background="new 8 8 16 16" xml:space="preserve"><rect x="10.75" y="10.125" width="40.5" height="20.5" fill="#00ff00" /></svg>').appendTo('#loops');
+    $('<svg version="1.1" style="display:none" id="play_seq_button" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="8 8 16 16" enable-background="new 8 8 16 16" xml:space="preserve"><rect x="10.75" y="10.125" width="40.5" height="20.5" fill="blue" /></svg>').appendTo('#loops');
     $('#credits ul').append('<li><div id="bpmSettings" /></li>');
     $('<label for="bpm">bpm: </label>').appendTo('#bpmSettings');
     $('<input/>').attr({ type: 'text', size: '4', id: 'bpm', name: 'bpm', value: '120'}).appendTo('#bpmSettings');
@@ -21,7 +29,6 @@ $(function() {
     $('<span id="metro_trigger">&#9833</span> <span id="signature"></span>/4 drum pattern, tempo <span id="tempo"></span> BPM').appendTo('#sequencerText-menu');
     $('#credits ul').append('<li><div id="sequencerbox" /></li>');
     $('<table id="pattern"><tr data-track="hihat"></tr><tr data-track="graphics"></tr></table>').appendTo('#sequencerbox');
-
 
   // http://www.reddit.com/r/Music/comments/21cs3i/fun_beat_mixer_in_your_browser_just_press_keys_on/cgcfxxg
     // originally from (c) 2014 Nicen Jehr
@@ -87,6 +94,10 @@ $(function() {
           if (e.target.id === 'bpm') {
             return false;
           }
+          // ignore the tab
+          if (e.which == 9 || e.which == 91) {
+            return false;
+          }
           bpm = bpmInput.val();
           console.log("Current bpm: " + bpm + "at SEQ.beatCount: " + SEQ.beatCount);
           startBeat('graphics', SEQ.beatCount, e.which);
@@ -107,6 +118,8 @@ $(function() {
     $(window).on('keydown', onKeyDown);
     $('#lp_recpause').bind('click', {}, function(event) {
         $('#lp_stop').show();
+        $('#new_seq_button').show();
+        $('#play_seq_button').show();
         if (is_recording) {
             is_recording = false;
             $(event.target).attr('fill', '#cccccc');
@@ -139,6 +152,54 @@ $(function() {
       mute_metronome = true;
       $('#metro_trigger')[0].style.color = 'red';
     }
+  });
+
+  $('#new_seq_button').bind('click', {}, function(event) {
+    var beatAssets = []
+    for (var beatInd = 0; beatInd < signature; beatInd++) {
+      var event = beats['graphics'][beatInd]
+      if (event != null) {
+        var asset = event.asset;
+        SEQ.currentBar = SEQ.currentBar + 1;
+        beatAssets[beatInd] = asset
+        event.clear()
+      }
+    }
+    if (beatAssets.length > 0) {
+      doc = {sessionName: SEQ.sessionName, currentBar: SEQ.currentBar, beatAssets:beatAssets};
+      db.post(doc, function(err, response) {
+        if (err) {
+          console.log("err: " + err)
+        } else {
+          console.log("Clearing current bar.")
+        }
+      });
+    }
+
+//    db.allDocs({include_docs: true}, function(err, response) {
+//      if (err) {
+//        console.log("err: " + err)
+//      } else {
+//        console.log("Here are some docs.")
+//      }
+//    });
+
+  });
+
+  $('#play_seq_button').bind('click', {}, function(event) {
+
+    function map(doc) {
+      if(doc.sessionName) {
+        emit(doc.sessionName, null);
+      }
+    }
+
+    db.query({map: map}, {reduce: false}, function(err, response) {
+
+//      simulateKeypress(which)
+
+    });
+
   });
 
   // Function for moving the beat cursor
